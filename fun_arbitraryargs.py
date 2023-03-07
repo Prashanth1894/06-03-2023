00f42f5cab6509bf432cf5e64691286a128887a1
Prashanth1894 <126759450+Prashanth1894@users.noreply.github.com>
2023-03-07 11:45:11 +0530

    Add files via upload

---
+++ fun_arbitraryargs.py
@@ -0,0 +1,4 @@
+def my_function(*names):
+  print("The big name is " + names[0])
+
+my_function("prashanth", "ram", "rahul")
---
+++ keyword_args.py
@@ -0,0 +1,4 @@
+def my_function(name, age, qualification):
+  print(" is " + name)
+
+my_function(name = "prashanth", age = "25", qualification = "Btech")
